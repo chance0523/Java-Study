3c3cb0157d337d39c1ea488cbc9da71f01310a9b
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-07 23:48:27 +0900

    [Spring-Core] 관심사 분리 - AppConfig

--- spring-core/src/test/java/hello/core/member/MemberServiceTest.java
+++ spring-core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,6 +1,9 @@
 package hello.core.member;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import hello.core.AppConfig;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,7 +12,13 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
--- spring-core/src/test/java/hello/core/order/OrderServiceTest.java
+++ spring-core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,11 +1,12 @@
 package hello.core.order;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,8 +15,15 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {

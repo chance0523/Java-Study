c407184b90a987c8789666cb75b8012aca9a895d
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-20 01:20:34 +0900

    [Spring-Core] 다양한 설정 형식 지원 - 자바 코드, XML

---
+++ spring-core/src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,22 @@
+package hello.core.xml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import hello.core.member.MemberService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author JeongJoon Seo
+ */
+class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}

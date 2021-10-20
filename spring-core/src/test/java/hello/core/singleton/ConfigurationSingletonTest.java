ed87aba41b63a0fa9215b9ee9a6ae464da816dc1
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-21 00:10:18 +0900

    [Spring-Core] @Configuration과 바이트코드 조작의 마법

--- spring-core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ spring-core/src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -33,4 +33,14 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        // AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$82da700f
+    }
 }

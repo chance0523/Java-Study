8fe760231cf342d66bfaaa908816caf85c9396be
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-26 10:06:30 +0900

    [Spring-Core] 중복 등록과 충돌 - rollback

--- spring-core/src/main/java/hello/core/AutoAppConfig.java
+++ spring-core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,12 +1,8 @@
 package hello.core;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
 
 /**
  * @author JeongJoon Seo
@@ -14,9 +10,4 @@ import hello.core.member.MemoryMemberRepository;
 @Configuration
 @ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
-
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
 }

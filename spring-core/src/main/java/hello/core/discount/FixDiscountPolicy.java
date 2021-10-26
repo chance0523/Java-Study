cdafe21a28251d7c839c306a3a37a1cb99019965
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-27 00:43:57 +0900

    [Spring-Core] 조회한 빈이 모두 필요할 때, List, Map

--- spring-core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ spring-core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,11 +1,14 @@
 package hello.core.discount;
 
+import org.springframework.stereotype.Component;
+
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
 /**
  * @author JeongJoon Seo
  */
+@Component
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000; // 1000원 할인

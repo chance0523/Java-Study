34a7ff31956795c9ec0ad885ec047cf017637ca1
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-27 00:06:23 +0900

    [Spring-Core] lombok 추가

--- spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -13,18 +13,8 @@ import hello.core.member.MemberRepository;
 @Component
 public class OrderServiceImpl implements OrderService {
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy;
-
-    @Autowired
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
-    }
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {

7884f65688ff1876faba230160319ca0f0153413
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-10-27 00:09:09 +0900

    [Spring-Core] lombok 적용

--- spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ spring-core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,26 +1,21 @@
 package hello.core.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author JeongJoon Seo
  */
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

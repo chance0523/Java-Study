cbd057245b6c6e6921152184653a59df29c7f950
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-03 22:07:30 +0900

    [Spring-Core] 애노테이션 @PostConstruct, @PreDestroy

--- spring-core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ spring-core/src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -21,7 +21,7 @@ class BeanLifeCycleTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");

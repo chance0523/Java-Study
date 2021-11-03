88d394ebcfa00553426f759b895f5d1435b36f8d
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-03 23:42:48 +0900

    [Spring-Core] 스코프와 Provider

--- spring-core/src/main/java/hello/core/logdemo/LogDemoService.java
+++ spring-core/src/main/java/hello/core/logdemo/LogDemoService.java
@@ -1,5 +1,6 @@
 package hello.core.logdemo;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 import hello.core.common.MyLogger;
@@ -12,9 +13,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     public void logic(String id) {
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
--- spring-core/src/main/java/hello/core/web/LogDemoController.java
+++ spring-core/src/main/java/hello/core/web/LogDemoController.java
@@ -1,5 +1,6 @@
 package hello.core.web;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,12 +19,13 @@ import lombok.RequiredArgsConstructor;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
         logDemoService.logic("testId");

6c89d2dc3686d4c9fbb6c036c9f78ab6401c359e
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-03 23:51:00 +0900

    [Spring-Core] 스코프와 프록시

--- spring-core/src/main/java/hello/core/common/MyLogger.java
+++ spring-core/src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
@@ -12,7 +13,7 @@ import javax.annotation.PreDestroy;
  * @author JeongJoon Seo
  */
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid;

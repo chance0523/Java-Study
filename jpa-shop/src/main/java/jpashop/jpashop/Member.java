6d0b92e608cda8c21df997d06fd07c90b7fd4459
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-08 00:23:17 +0900

    [Spring-JPA1] JPA와 DB 설정, 동작확인

---
+++ jpa-shop/src/main/java/jpashop/jpashop/Member.java
@@ -0,0 +1,23 @@
+package jpashop.jpashop;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author JeongJoon Seo
+ */
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+
+}
--- jpa-shop/src/test/java/jpashop/jpashop/JpashopApplicationTests.java
+++ jpa-shop/src/test/java/jpashop/jpashop/JpashopApplicationTests.java
@@ -1,13 +1,13 @@
 package jpashop.jpashop;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JpashopApplicationTests {
+public class JpashopApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 
 }

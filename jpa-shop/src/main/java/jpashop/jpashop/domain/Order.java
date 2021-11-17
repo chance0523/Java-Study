c820e82ece09f8288ccf4985ba918dd542563290
서정준/샵바이주문파트/GD <jeongjoon-seo@godo.co.kr>
2021-11-18 08:52:19 +0900

    [Spring-JPA1] 주문 서비스 개발

--- jpa-shop/src/main/java/jpashop/jpashop/domain/Order.java
+++ jpa-shop/src/main/java/jpashop/jpashop/domain/Order.java
@@ -68,7 +68,7 @@ public class Order {
     }
 
     //==생성 메서드==//
-    public static Order createOrder(Member member, Delivery delivery, List<OrderItem> orderItems) {
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
         Order order = new Order();
         order.setMember(member);
         order.setDelivery(delivery);
---
+++ jpa-shop/src/main/java/jpashop/jpashop/service/OrderService.java
@@ -0,0 +1,66 @@
+package jpashop.jpashop.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jpashop.jpashop.domain.Delivery;
+import jpashop.jpashop.domain.DeliveryStatus;
+import jpashop.jpashop.domain.Member;
+import jpashop.jpashop.domain.Order;
+import jpashop.jpashop.domain.OrderItem;
+import jpashop.jpashop.domain.item.Item;
+import jpashop.jpashop.repository.ItemRepository;
+import jpashop.jpashop.repository.MemberRepository;
+import jpashop.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author JeongJoon Seo
+ */
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+    /**
+     * 주문
+     */
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+        //엔티티 조회
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+        //배송정보 생성
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+        //주문상품 생성
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+        //주문 생성
+        Order order = Order.createOrder(member, delivery, orderItem);
+        //주문 저장
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    /**
+     * 주문 취소
+     */
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        //주문 엔티티 조회
+        Order order = orderRepository.findOne(orderId);
+        //주문 취소
+        order.cancel();
+    }
+    /** 주문 검색 */
+/*
+ public List<Order> findOrders(OrderSearch orderSearch) {
+ return orderRepository.findAll(orderSearch);
+ }
+*/
+}
